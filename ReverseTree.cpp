c7db9124427ae7b692b7a83d928682f0df06f165
itaybublil <itaybublil13@gmail.com>
2026-01-21 20:16:09 +0200

    Add ReverseTree and clean project files

---
+++ ReverseTree.cpp
@@ -0,0 +1,5 @@
+//
+// Created by itayb on 21/01/2026.
+//
+
+#include "ReverseTree.h"
---
+++ ReverseTree.h
@@ -0,0 +1,20 @@
+//
+// Created by itayb on 21/01/2026.
+//
+
+#ifndef DATA_STRUCTURES_WET_2_REVERSE_TREE_H
+#define DATA_STRUCTURES_WET_2_REVERSE_TREE_H
+struct Hunter{
+	const NenAbility &nenAbility
+}
+
+struct TreeNode {
+
+}
+
+class ReverseTree {
+
+};
+
+
+#endif //DATA_STRUCTURES_WET_2_REVERSE_TREE_H
