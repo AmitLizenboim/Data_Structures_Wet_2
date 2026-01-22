832c452f03eebdc7f8232ca449be2598005cfb63
itaybublil <itaybublil13@gmail.com>
2026-01-22 21:19:04 +0200

    Add ReverseTree include Union-Find

--- ReverseTree.cpp
+++ ReverseTree.cpp
@@ -3,3 +3,87 @@
 //
 
 #include "ReverseTree.h"
+
+ReverseTree::ReverseTree() : root(nullptr), size(0), sumNenAbility(new NenAbility(NenAbility::zero())) {}
+ReverseTree::~ReverseTree() {
+    root = nullptr;
+    delete sumNenAbility;
+}
+HunterNode* ReverseTree::insert(const NenAbility &nenAbility, int fightsHad) {
+    HunterNode* newNode = new HunterNode(nenAbility, fightsHad, root);
+    size++;
+    *newNode->bonusNenAbility += *sumNenAbility;
+    if (root != nullptr) {
+        *newNode->bonusNenAbility -= *root->bonusNenAbility;
+        newNode->bonusFights -= root ->bonusFights;
+    }
+    *sumNenAbility += nenAbility;
+    return newNode;
+}
+ReverseTree* ReverseTree::Union(ReverseTree &other) {
+    if (other.root == nullptr) {
+        return this;
+    }
+    if (size >= other.size) {
+        other.root -> parent = root;
+        *other.root ->bonusNenAbility += (*sumNenAbility - *root->bonusNenAbility);
+        other.root->bonusFights -= root ->bonusFights;
+    } else {
+        root -> parent = other.root;
+        *other.root ->bonusNenAbility += *sumNenAbility;
+        *root -> bonusNenAbility -= *other.root->bonusNenAbility;
+        root->bonusFights -= other.root ->bonusFights;
+        root = other.root;
+    }
+    size += other.size;
+    *sumNenAbility += *other.sumNenAbility;
+    return this;
+}
+void ReverseTree::die() {
+    root ->alive = false;
+}
+
+bool ReverseTree::isEmpty() {
+    return (root == nullptr);
+}
+
+void pathCompression(HunterNode *hunter, HunterNode *root) {
+    if (hunter == nullptr || hunter == root || hunter->parent == root) {
+        return;
+    }
+    pathCompression(hunter->parent, root);
+    hunter ->bonusFights += hunter->parent->bonusFights;
+    *hunter ->bonusNenAbility += *hunter->parent->bonusNenAbility;
+    hunter ->parent = root;
+}
+
+HunterNode* findRoot(HunterNode *hunter) {
+    if (hunter == nullptr) {
+        return nullptr;
+    }
+    if (hunter->parent == nullptr) {
+        return hunter;
+    }
+    return findRoot(hunter->parent);
+}
+
+int findFights(HunterNode *hunter) {
+    if (hunter == nullptr) {
+        return 0;
+    }
+    pathCompression(hunter, findRoot(hunter));
+    if (hunter -> parent == nullptr) {
+        return hunter->fightsHad + hunter->bonusFights;
+    }
+    return hunter->fightsHad + hunter->bonusFights + hunter->parent->bonusFights;
+}
+NenAbility findPartialNenAbility(HunterNode *hunter) {
+    if (hunter == nullptr) {
+        return NenAbility::zero();
+    }
+    pathCompression(hunter, findRoot(hunter));
+    if (hunter -> parent == nullptr) {
+        return (*hunter->bonusNenAbility + *hunter->nenAbility);
+    }
+    return (*hunter->bonusNenAbility + *hunter->nenAbility + *hunter->parent->bonusNenAbility);
+}
--- ReverseTree.h
+++ ReverseTree.h
@@ -1,20 +1,41 @@
 //
 // Created by itayb on 21/01/2026.
 //
-
+#include "wet2util.h"
 #ifndef DATA_STRUCTURES_WET_2_REVERSE_TREE_H
 #define DATA_STRUCTURES_WET_2_REVERSE_TREE_H
-struct Hunter{
-	const NenAbility &nenAbility
-}
 
-struct TreeNode {
+struct HunterNode {
+	const NenAbility *nenAbility;
+	const int fightsHad;
+	HunterNode *parent;
+	int bonusFights;
+	NenAbility *bonusNenAbility;
+	bool alive;
 
-}
-
-class ReverseTree {
-
+	HunterNode(const NenAbility &nenAbility, int fightsHad, HunterNode *parent): nenAbility(&nenAbility),fightsHad(fightsHad),
+	parent(parent), bonusFights(0), bonusNenAbility(new NenAbility(NenAbility::zero())), alive(true){}
+	~HunterNode() {delete nenAbility; delete bonusNenAbility;}
 };
 
+class ReverseTree {
+	private:
+		HunterNode *root;
+		int size;
+		NenAbility *sumNenAbility;
+
+	public:
+		ReverseTree();
+		~ReverseTree();
+		ReverseTree(const ReverseTree &other) = delete;
+		ReverseTree &operator=(const ReverseTree &other) = delete;
+		HunterNode* insert(const NenAbility &nenAbility, int fightsHad);
+		ReverseTree* Union(ReverseTree &other);
+		void die();
+		bool isEmpty();
+};
+
+int findFights(HunterNode *hunter);
+NenAbility findPartialNenAbility(HunterNode *hunter);
 
 #endif //DATA_STRUCTURES_WET_2_REVERSE_TREE_H
